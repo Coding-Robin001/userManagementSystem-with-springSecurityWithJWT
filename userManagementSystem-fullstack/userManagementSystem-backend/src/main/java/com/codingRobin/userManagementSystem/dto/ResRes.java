c1e951c9f3ff3974ed1bdfcf931e094a89c21e29
Coding-Robin001 <codingade@gmail.com>
2024-12-21 09:57:01 +0000

    created dto class

---
+++ userManagementSystem-fullstack/userManagementSystem-backend/src/main/java/com/codingRobin/userManagementSystem/dto/ResRes.java
@@ -0,0 +1,28 @@
+package com.codingRobin.userManagementSystem.dto;
+
+import com.codingRobin.userManagementSystem.entity.OurUsers;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ResRes {
+
+    private int statusCode;
+    private String error;
+    private String message;
+    private String token;
+    private String refreshToken;
+    private String expirationTime;
+    private String city;
+    private String name;
+    private String role;
+    private String email;
+    private String password;
+    private OurUsers ourUsers;
+    private List<OurUsers> ourUsersList;
+}
