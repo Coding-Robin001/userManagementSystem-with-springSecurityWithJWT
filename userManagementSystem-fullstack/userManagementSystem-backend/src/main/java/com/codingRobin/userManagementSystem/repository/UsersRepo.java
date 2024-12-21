fc1f398de6d85ec90e1407b140c52e2d9c770347
Coding-Robin001 <codingade@gmail.com>
2024-12-21 10:03:59 +0000

    commmit

---
+++ userManagementSystem-fullstack/userManagementSystem-backend/src/main/java/com/codingRobin/userManagementSystem/repository/UsersRepo.java
@@ -0,0 +1,12 @@
+package com.codingRobin.userManagementSystem.repository;
+
+
+import com.codingRobin.userManagementSystem.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+}
