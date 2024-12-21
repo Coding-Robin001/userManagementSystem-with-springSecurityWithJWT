ee05cf20f5282a4e9c179b9e713b5f331f7bca78
Coding-Robin001 <codingade@gmail.com>
2024-12-21 11:12:30 +0000

    method to extract payload from token string and check if token is valid or expired

--- userManagementSystem-fullstack/userManagementSystem-backend/src/main/java/com/codingRobin/userManagementSystem/service/JwtUtils.java
+++ userManagementSystem-fullstack/userManagementSystem-backend/src/main/java/com/codingRobin/userManagementSystem/service/JwtUtils.java
@@ -1,5 +1,6 @@
 package com.codingRobin.userManagementSystem.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -10,12 +11,13 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.function.Function;
 
 
 @Component
 public class JwtUtils {
 
-    private SecretKey key;
+    private final SecretKey key;
     private static final long EXP_TIME = 8640000L;
 
     public JwtUtils(){
@@ -41,6 +43,24 @@ public class JwtUtils {
                 .expiration(new Date(System.currentTimeMillis() + EXP_TIME))
                 .signWith(key)
                 .compact();
+    }
+
+    public String extractUserName(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction) {
+        return claimsTFunction.apply(Jwts.parser().verifyWith(key)
+                .build().parseSignedClaims(token).getPayload());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String userName = extractUserName(token);
+        return userName.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token, Claims::getExpiration).before(new Date());
     }
 }
 
