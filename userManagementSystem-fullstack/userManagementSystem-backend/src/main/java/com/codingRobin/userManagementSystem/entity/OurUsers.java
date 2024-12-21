c5103fedb4c94821c9de43d57431d6c36463c28e
Coding-Robin001 <codingade@gmail.com>
2024-12-21 09:45:37 +0000

    commit

--- userManagementSystem-fullstack/userManagementSystem-backend/src/main/java/com/codingRobin/userManagementSystem/entity/OurUsers.java
+++ userManagementSystem-fullstack/userManagementSystem-backend/src/main/java/com/codingRobin/userManagementSystem/entity/OurUsers.java
@@ -2,13 +2,55 @@ package com.codingRobin.userManagementSystem.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name="ourUsers")
 @Data
-public class OurUsers {
+public class OurUsers implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String email;
+    private String name;
+    private String password;
+    private String role;
+    private String city;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+//        return true;
+    }
 }
