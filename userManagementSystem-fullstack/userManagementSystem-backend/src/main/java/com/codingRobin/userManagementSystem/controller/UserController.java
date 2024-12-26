8ffe0897e7341b505b15b24804ba560e64bb329e
Coding-Robin001 <codingade@gmail.com>
2024-12-26 12:05:19 +0000

    added controller logic for refreshToken getUserById updateUser getProfile

--- userManagementSystem-fullstack/userManagementSystem-backend/src/main/java/com/codingRobin/userManagementSystem/controller/UserController.java
+++ userManagementSystem-fullstack/userManagementSystem-backend/src/main/java/com/codingRobin/userManagementSystem/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.codingRobin.userManagementSystem.controller;
 
 import com.codingRobin.userManagementSystem.dto.ReqRes;
+import com.codingRobin.userManagementSystem.entity.OurUsers;
 import com.codingRobin.userManagementSystem.service.UsersManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,18 +16,18 @@ public class UserController {
     private UsersManagementService usersManagementService;
 
     @PostMapping("/auth/register")
-    public ResponseEntity<ReqRes> register(@RequestBody ReqRes reg){
-        return ResponseEntity.ok(usersManagementService.register(reg));
+    public ResponseEntity<ReqRes> register(@RequestBody ReqRes regResData){
+        return ResponseEntity.ok(usersManagementService.register(regResData));
     }
 
     @PostMapping("/auth/login")
-    public ResponseEntity<ReqRes> login(@RequestBody ReqRes reg){
-        return ResponseEntity.ok(usersManagementService.login(reg));
+    public ResponseEntity<ReqRes> login(@RequestBody ReqRes regResData){
+        return ResponseEntity.ok(usersManagementService.login(regResData));
     }
 
     @PostMapping("/auth/refreshToken")
-    public ResponseEntity<ReqRes> refreshToken(@RequestBody ReqRes reg){
-        return ResponseEntity.ok(usersManagementService.refreshToken(reg));
+    public ResponseEntity<ReqRes> refreshToken(@RequestBody ReqRes regResData){
+        return ResponseEntity.ok(usersManagementService.refreshToken(regResData));
     }
 
     @GetMapping("/admin/getAllUsers")
@@ -37,6 +40,17 @@ public class UserController {
         return ResponseEntity.ok(usersManagementService.getUserById(userId));
     }
 
+    @GetMapping("/admin/updateUser/{userId}")
+    public ResponseEntity<ReqRes> updateUserById(@PathVariable Integer userId, @RequestBody OurUsers regResData){
+        return ResponseEntity.ok(usersManagementService.updateUser(userId, regResData));
+    }
 
+    @GetMapping("/adminuser/getProfile")
+    public ResponseEntity<ReqRes> getProfile(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        ReqRes response = usersManagementService.getMyInfo(email);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
 
 }
