c9533f9a92bdf2b05b922b8053850c80923e10e0
Coding-Robin001 <codingade@gmail.com>
2025-01-12 10:41:48 +0000

    created navbar and functionality

--- userManagementSystem-fullstack/userManagementSystem-frontend/src/component/navigation/navbar/Navbar.jsx
+++ userManagementSystem-fullstack/userManagementSystem-frontend/src/component/navigation/navbar/Navbar.jsx
@@ -8,31 +8,43 @@ const Navbar = () => {
     const isAuthenticated = UserService.isAuthenticated()
     const isAdmin = UserService.isAdmin()
 
+    const handleLogout = () => {
+        const confirmDeleteToken = window.confirm("are you sure you want to logout this user?")
+        if (confirmDeleteToken) {
+            UserService.logout();
+        }
+    }
     return (
         <div className='navbar-container'>
             <div className='nav-list'>
-                <Link to="/">
-                    <div className='nav_logo'>
-                        <img src="https://ik.imagekit.io/zn8ucbmpm/global5050%20assets/WhatsApp%20Image%202024-12-02%20at%2015.40.16.jpeg?updatedAt=1733242095759" alt="" />
-                    </div>
-                </Link>
-                {/* <h1>GLOBAL 50/50</h1> */}
-                <div className='sub-nav'>
-                    <Link to="/home">
-                        <p>Home</p>
+                {!isAuthenticated &&
+                    <Link to="/">
+                        <div className='nav_logo'>
+                            CODING ROBIN
+                        </div>
                     </Link>
-                    <Link to="/about">
+                }
+                {
+                    isAuthenticated &&
+                    <Link to="/profile">
+                        <p>profile</p>
+                    </Link>
+                }
+                {
+                    isAdmin &&
+                    <Link to="/admin/userManagement">
+                        <p>user management</p>
+                    </Link>
+                }
+                {
+                    isAuthenticated &&
+                    <Link
+                        to="/"
+                        onClick={handleLogout}
+                    >
                         <p>About Us</p>
                     </Link>
-                    <Link to="/contact">
-                        <p>Contact</p>
-                    </Link>
-                </div>
-                {/* <div className='btn-box'> */}
-                <button className='nav-btn'>
-                    Contact Us
-                </button>
-                {/* </div> */}
+                }
             </div>
 
         </div>
--- userManagementSystem-fullstack/userManagementSystem-frontend/src/component/usersPage/ProfilePage.jsx
+++ userManagementSystem-fullstack/userManagementSystem-frontend/src/component/usersPage/ProfilePage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import UserService from '../service/UserService'
+import { Link } from 'react-router-dom'
 
 const ProfilePage = () => {
+  const [profileInfo, setProfileInfo] = useState({})
+
+  useEffect(() => {
+    fetchProfileInfo()
+  }, [])
+
+  const fetchProfileInfo = async () => {
+    const token = localStorage.getItem("token")
+    const response = await UserService.getProfileInfo(token)
+    setProfileInfo(response.ourUsers)
+  }
+
   return (
     <div>
-      
+
     </div>
   )
 }
