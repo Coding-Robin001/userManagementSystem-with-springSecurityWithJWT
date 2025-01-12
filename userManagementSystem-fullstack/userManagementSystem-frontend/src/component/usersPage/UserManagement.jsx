35b2f4bee1812256e874008699404d4a08146f45
Coding-Robin001 <codingade@gmail.com>
2025-01-12 11:49:55 +0000

    user management page and functionality

--- userManagementSystem-fullstack/userManagementSystem-frontend/src/component/usersPage/UserManagement.jsx
+++ userManagementSystem-fullstack/userManagementSystem-frontend/src/component/usersPage/UserManagement.jsx
@@ -15,15 +15,66 @@ const UserManagement = () => {
       const token = localStorage.getItem("token")
       const response = await UserService.getAllUsers(token)
       setUsers(response.ourUsersList)
-      
+
     } catch (error) {
       console.log("error fetching user", error);
     }
   }
 
+  const deleteUser = async (userId) => {
+
+    try {
+      const confirmDeleteUser = window.confirm("are you sure yu want to delete user?")
+      const token = localStorage.getItem("token")
+      if (confirmDeleteUser) {
+        await UserService.deleteUser(userId)
+        fetchUsers()
+      }
+
+    } catch (error) {
+      console.log("error deleting user", error);
+    }
+  }
 
   return (
     <div>
+      <h2>User Management Page</h2>
+      <button><Link to="/register">Add User</Link></button>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>EMAIL</th>
+            <th>ACTIONS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map(user => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete User
+                  </button>
+                  <button>
+                    <Link
+                      to={`/updateUser/${user.id}`}
+                    >
+                      Update User
+                    </Link>
+                  </button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
 
     </div>
   )
